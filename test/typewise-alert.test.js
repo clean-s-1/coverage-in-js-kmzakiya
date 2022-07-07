e492a16e02ac69a52d78a86cabf4513511e240c4
Zakiya <31302217+kmzakiya@users.noreply.github.com>
2022-07-07 20:20:31 +0530

    Update typewise-alert.test.js

--- test/typewise-alert.test.js
+++ test/typewise-alert.test.js
@@ -4,3 +4,9 @@ const {expect} = require('chai');
 it('infers a value lower than the minimum as TOO_LOW', () => {
   expect(alerts.inferBreach(20, 50, 100)).equals('TOO_LOW');
 });
+it('infers a value lower than the maximum as TOO_HIGH', () => {
+  expect(alerts.inferBreach(120, 50, 100)).equals('TOO_HIGH');
+});
+it('infers a value between minimum and maximum as NORMAL', () => {
+  expect(alerts.inferBreach(70, 50, 100)).equals('NORMAL');
+});
