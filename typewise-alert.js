866cc41ac56846d2bd8f27406ed5780bcfee5e9f
Zakiya <31302217+kmzakiya@users.noreply.github.com>
2022-07-07 20:21:24 +0530

    Update typewise-alert.js

--- typewise-alert.js
+++ typewise-alert.js
@@ -1,35 +1,32 @@
+const COOLING_LIMITS = {
+  PASSIVE_COOLING: {lowerLimit: 0, upperLimit: 35},
+  HI_ACTIVE_COOLING: {lowerLimit: 0, upperLimit: 45},
+  MED_ACTIVE_COOLING: {lowerLimit: 0, upperLimit: 40},
+};
 
 function inferBreach(value, lowerLimit, upperLimit) {
   if (value < lowerLimit) {
     return 'TOO_LOW';
-  }
-  if (value > upperLimit) {
+  } else if (value > upperLimit) {
     return 'TOO_HIGH';
   }
   return 'NORMAL';
 }
 
-function classifyTemperatureBreach(coolingType, temperatureInC) {
-  let lowerLimit = 0;
-  let upperLimit = 0;
-  if (coolingType == 'PASSIVE_COOLING') {
-    lowerLimit = 0;
-    upperLimit = 35;
-  } else if (coolingType == 'HI_ACTIVE_COOLING') {
-    lowerLimit = 0;
-    upperLimit = 45;
-  } else if (coolingType == 'MED_ACTIVE_COOLING') {
-    lowerLimit = 0;
-    upperLimit = 40;
-  }
+function classifyTemperatureBreach(coolingType, temperatureInC, limits) {
+  const {lowerLimit, upperLimit} = limits[coolingType];
   return inferBreach(temperatureInC, lowerLimit, upperLimit);
 }
 
 function checkAndAlert(alertTarget, batteryChar, temperatureInC) {
-  const breachType = classifyTemperatureBreach(batteryChar['coolingType'], temperatureInC);
-  if (alertTarget == 'TO_CONTROLLER') {
+  const breachType = classifyTemperatureBreach(
+    batteryChar["coolingType"],
+    temperatureInC,
+    COOLING_LIMITS
+  );
+  if (alertTarget == "TO_CONTROLLER") {
     sendToController(breachType);
-  } else if (alertTarget == 'TO_EMAIL') {
+  } else if (alertTarget == "TO_EMAIL") {
     sendToEmail(breachType);
   }
 }
@@ -40,15 +37,20 @@ function sendToController(breachType) {
 }
 
 function sendToEmail(breachType) {
-  const recepient = 'a.b@c.com';
-  if (breachType == 'TOO_LOW') {
+  const recepient = "a.b@c.com";
+  if (breachType == "TOO_LOW") {
     console.log(`To: ${recepient}`);
-    console.log('Hi, the temperature is too low');
-  } else if (breachType == 'TOO_HIGH') {
+    console.log("Hi, the temperature is too low");
+  } else if (breachType == "TOO_HIGH") {
     console.log(`To: ${recepient}`);
-    console.log('Hi, the temperature is too high');
+    console.log("Hi, the temperature is too high");
   }
 }
 
-module.exports =
-    {inferBreach, classifyTemperatureBreach, checkAndAlert, sendToController, sendToEmail};
+module.exports = {
+  inferBreach,
+  classifyTemperatureBreach,
+  checkAndAlert,
+  sendToController,
+  sendToEmail,
+};
