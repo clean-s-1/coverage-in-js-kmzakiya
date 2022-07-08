ab3e6a25275ae6acd73d4de6b6f968eb873b615c
Zakiya <31302217+kmzakiya@users.noreply.github.com>
2022-07-08 10:51:34 +0530

    Update typewise-alert.test.js

--- test/typewise-alert.test.js
+++ test/typewise-alert.test.js
@@ -14,13 +14,16 @@ describe("inferBreach", () => {
 });
 describe("classifyTemperatureBreach", () => {
   it("return TOO_LOW for a value lower than the minimum", () => {
-    expect(alerts.classifyTemperatureBreach('PASSIVE_COOLING', -30, alerts.COOLING_LIMITS)).equals("TOO_LOW");
+    expect(alerts.classifyTemperatureBreach('PASSIVE_COOLING', -30, alerts.COOLING_LIMITS))
+    .equals("TOO_LOW");
   });
   it("return TOO_HIGH for a value lower than the maximum", () => {
-    expect(alerts.classifyTemperatureBreach('PASSIVE_COOLING', 50, alerts.COOLING_LIMITS)).equals("TOO_HIGH");
+    expect(alerts.classifyTemperatureBreach('PASSIVE_COOLING', 50, alerts.COOLING_LIMITS))
+    .equals("TOO_HIGH");
   });
   it("return NORMAL for a value between minimum and maximum", () => {
-    expect(alerts.classifyTemperatureBreach('PASSIVE_COOLING', 20, alerts.COOLING_LIMITS)).equals("NORMAL");
+    expect(alerts.classifyTemperatureBreach('PASSIVE_COOLING', 20, alerts.COOLING_LIMITS))
+    .equals("NORMAL");
   });
 });
 
